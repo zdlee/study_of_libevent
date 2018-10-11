a53e6190a79d0de07ccc157fe12840981d9c3781
yanhui03 <yanhui03@baidu.com>
2018-10-11 16:10:49 +0800

    error handle

--- practice/c_api/client.c
+++ practice/c_api/client.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include "wrap.h"
 
 #define SERVER_PORT 7777
 
@@ -15,23 +16,23 @@ int main(int argc, char const *argv[])
     char buf[BUFSIZ];
     int len;
 
-    cfd = socket(AF_INET, SOCK_STREAM, 0);
+    cfd = Socket(AF_INET, SOCK_STREAM, 0);
 
     bzero(&server_addr, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     server_addr.sin_port = htons(SERVER_PORT);
 
-    connect(cfd, (struct sockaddr*) &server_addr, sizeof(server_addr));
+    Connect(cfd, (struct sockaddr*) &server_addr, sizeof(server_addr));
 
     while(1) {
         fgets(buf, sizeof(buf), stdin);
         write(cfd, buf, strlen(buf));
-        len = read(cfd, buf, sizeof(buf));
-        write(STDOUT_FILENO, buf, len);
+        len = Read(cfd, buf, sizeof(buf));
+        Write(STDOUT_FILENO, buf, len);
     }
 
-    close(cfd);
+    Close(cfd);
 
     return 0;
 }
--- practice/c_api/server.c
+++ practice/c_api/server.c
@@ -7,6 +7,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <ctype.h>
+#include "wrap.h"
 
 #define SERVER_PORT 7777
 
@@ -18,36 +19,36 @@ int main(int argc, char const *argv[])
     char client_IP[BUFSIZ], buf[BUFSIZ];
     int len, i;
 
-    sfd = socket(AF_INET, SOCK_STREAM, 0);
+    sfd = Socket(AF_INET, SOCK_STREAM, 0);
 
     bzero(&server_addr, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     server_addr.sin_port = htons(SERVER_PORT);
 
-    bind(sfd, (struct sockaddr*) &server_addr, sizeof(server_addr));
+    Bind(sfd, (struct sockaddr*) &server_addr, sizeof(server_addr));
 
-    listen(sfd, 1024);
+    Listen(sfd, 1024);
 
     printf("waiting for connect...");
 
     client_addr_len = sizeof(client_addr);
-    cfd = accept(sfd, (struct sockaddr*) &client_addr, &client_addr_len);
+    cfd = Accept(sfd, (struct sockaddr*) &client_addr, &client_addr_len);
 
     printf("client IP : %s\tport : %d\n", inet_ntop(AF_INET, &client_addr.sin_addr.s_addr, client_IP, sizeof(client_IP)),
         ntohs(client_addr.sin_port));
 
     while(1) {
-        len = read(cfd, buf, sizeof(buf));
-        write(STDOUT_FILENO, buf, len);
+        len = Read(cfd, buf, sizeof(buf));
+        Write(STDOUT_FILENO, buf, len);
         for(i = 0; i < len; i++){
             buf[i] = toupper(buf[i]);
         }
-        write(cfd, buf, len);
+        Write(cfd, buf, len);
     }
 
-    close(sfd);
-    close(cfd);
+    Close(sfd);
+    Close(cfd);
 
     return 0;
 }
